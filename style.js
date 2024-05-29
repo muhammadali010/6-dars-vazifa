44a4c445b945798f9ef3d03de70a37e0eb23ceff
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-05-29 13:51:29 +0500

    5 qolib ketti

---
+++ style.js
@@ -0,0 +1,61 @@
+// 1chi masala
+// let a = 50;
+// for (let i = 1; i <= a; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i * i);
+//     }
+// }
+
+
+// 2chi masala
+// let a = 10;
+// for (let i = 1; i <= a; i++) {
+//     console.log(i * i * i);
+// }
+
+
+// 3chi masala
+// let a = 20;
+// let result=0;
+// for (let i = 1; i <= a; i++) {
+// result+=i;
+// }
+// console.log(result);
+
+
+// 4chi masala
+// let a =30;
+// let result = 0;
+// for (let i = 1; i <= a; i++) {
+//     result += i * i;
+// }
+// console.log(result);
+
+
+// 6chi masala
+// let a = 100;
+// for (let i = 1; i <= a; i++){
+// if (i * 2 < a) {
+//     console.log(i);
+// }
+// }
+
+
+// 7chi masala
+// let a = 100;
+// for (let i = 1; i <= a; i++){
+//     if (i % 7 == 0 && i % 5 !== 0) {
+//         console.log(i);
+//     }
+//     }
+
+
+// 8chi masala
+// let a = + prompt("son kiriting");
+// let result = 0;
+// for (let i = 1; i <= a; i++) {
+//     if (a%i==0){
+//     result+=i;
+//     }
+// }
+// console.log(result);
